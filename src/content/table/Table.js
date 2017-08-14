0d0bad82ecccff90877deb3a1b2850ea159813c5
shiwenke <shiwenke@fiberhome.com>
2017-08-14 09:32:21 +0800

    打包，配置

---
+++ src/content/table/Table.js
@@ -0,0 +1,5 @@
+/**
+ * Created by Administrator on 2017/8/11.
+ */
+import React,{Component} from 'react'
+import Antd from 'antd'
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import DropDownBox from'./components/dropdownbox.js';
 import registerServiceWorker from './registerServiceWorker';
 import TopNav from './content/topnav/topnav.js';
 import LoginControl from './content/topnav/test.js';
-import Calculator from 'content/temperature/temperconvert.js';
+import Calculator from './content/temperature/temperconvert';
 import FilterableProductTable from './content/producttable/producttable.js';
 import TodoMVC from './content/todomvc/todoMVC';
 
